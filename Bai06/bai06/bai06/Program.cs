2cb6454ae5545b8dd0a7320944996b3b2a7d04af
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 14:26:25 +0700

    Tuan01/Bai06/Xay dung phuong thuc tim gia tri lon nhat cua ba so nguyen

---
+++ Bai06/bai06/bai06/Program.cs
@@ -0,0 +1,22 @@
+﻿// Xây dựng lớp có phương thức tìm giá trị lớn nhất của ba số nguyên.
+using System;
+
+namespace bai06
+{
+    class Program { 
+        public  static int Max(int a, int b, int c)
+        {
+            return (a > b && a > c) ? a : (b > c) ? b : c; //sử dụng toán tử điều kiện để tìm số lớn nhất
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhập số nguyên thứ nhất: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số nguyên thứ hai: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nhập số nguyên thứ ba: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Số lớn nhất là: " + Max(a, b, c)); //gọi phương thức Max để tìm số lớn nhất
+        }
+    }
+}
