3a4efb4072f67904f647032389a14fc31ef233c2
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 14:16:11 +0700

    Tuan 1/Bai 2/Nhap ho ten va xuat loi chao ra man hinh

---
+++ Bai02/bai02/bai02/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace bai02
+    {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ho va ten cua ban: ");
+            string hoTen = Console.ReadLine();
+            Console.WriteLine("Chao ban " + hoTen + "!");
+        }
+    }
+}
