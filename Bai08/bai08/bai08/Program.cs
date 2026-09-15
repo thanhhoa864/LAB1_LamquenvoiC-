ec125c9b3403726d0bc959323509f1e8ee88c427
Ging Pham <thanhhoa13052006@gmail.com>
2026-09-15 14:27:44 +0700

    Tuan01/Bai08/Xay dung phuong thuc hoan vi hai so thuc bang ref

---
+++ Bai08/bai08/bai08/Program.cs
@@ -0,0 +1,28 @@
+﻿//Xây dựng lớp có phương thức hoán vị hai số thực.
+
+using System;
+
+namespace bai08
+{
+    class Program
+    {
+        public static void HoanVi(ref double a, ref double b) // Phương thức hoán vị hai số thực
+
+        {
+            double temp = a; // Lưu giá trị của a vào biến tạm thời
+            a = b; // Gán giá trị của b cho a
+            b = temp; // Gán giá trị của biến tạm thời cho b
+        }
+    
+        public static void Main(string[] args)
+        {
+            Console.Write("Nhập số thực thứ nhất: ");
+            double num1 = double.Parse(Console.ReadLine());
+            Console.Write("Nhập số thực thứ hai: ");
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Trước khi hoán vị: "+num1+" "+num2); // In ra giá trị trước khi hoán vị
+            HoanVi(ref num1, ref num2);
+            Console.WriteLine("Sau khi hoán vị: "+num1+" "+num2); // In ra giá trị sau khi hoán vị
+        }
+    }
+}
